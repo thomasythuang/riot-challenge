60cee4e61029b8a2ac73747c064532cdb64beae8
thomasythuang <thomas.yt.huang@gmail.com>
2014-11-10 20:26:23 -0600

    Display champion abilities and effects

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -6,6 +6,7 @@ app.controller('mainController', function($scope, $http, Static){
 	// Default region = NA (NA > EU 5ever amirite)
 	$scope.region="na";
 	$scope.champions = [];
+	$scope.spells = [];
 
 	// Init function
 	$scope.onLoad = function(){
@@ -23,7 +24,6 @@ app.controller('mainController', function($scope, $http, Static){
 
 	$scope.getData = function(){
 		start();
-
 		// Load all champions
 		Static.getChamps($scope.region)
 			.success(function(data){
@@ -38,18 +38,90 @@ app.controller('mainController', function($scope, $http, Static){
 				console.log(status);
 				end();
 			});
-
 	};
 
 	function processData(){
-		console.log($scope.champions);
+		for (var i=0; i<$scope.champions.length; i++){
+			for (var j=0; j<$scope.champions[i].spells.length; j++){
+				var spell = $scope.champions[i].spells[j];
+				ratios(spell);
+				effects(spell);
+				spell.champ = $scope.champions[i].name;
+				$scope.spells.push(spell);
+			}
+		}
 		end();
+	}
+
+	function ratios(spell){
+		spell.ad = 0;
+		spell.ap = 0;
+		spell.bonusAd = 0;
+		spell.mana = 0;
+		var result = "";
+		var existingRatios = [];
+		if (spell.vars){
+			for (var i=0; i<spell.vars.length; i++){
+				parseRatio(spell.vars[i].link, spell.vars[i].coeff[0], spell);
+			}
+
+			if (spell.ad > 0)
+				result += "(+ " + spell.ad.toString() + " AD)";
+			if (spell.ap > 0)
+				result += "(+ " + spell.ap.toString() + " AP)";
+			if (spell.bonusAd > 0)
+				result += "(+ " + spell.bonusAd.toString() + " Bonus AD)";
+			if (spell.mana > 0)
+				result += "(+ " + spell.mana.toString() + " Mana)";
+		}
+		spell.ratios = result;
+	}
+	
+	function parseRatio(str, coeff, spell){
+		switch(str){
+			case "spelldamage":
+				spell.ap = coeff;
+				break;
+			case "attackdamage":
+				spell.ad = coeff;
+				break;
+			case "bonusattackdamage":
+				spell.bonusAd = coeff;
+				break;
+			case "mana":
+				spell.mana = coeff;
+				break;
+			default:
+				return false;
+		}
+	} 
+
+	function effects(spell){
+		var labels = ["Damage" , "Damage ", "Bonus Damage", "Base Damage", "Bonus Magic Damage", "Explosion Damage", "Damage per Second", "Damage Per Second", "Damage per Sec", "Damage dealt per second", "Base Damage per Second", "Max Damage", "Max Health Damage", "Heal", "Base Heal", "Health Restored", "Bonus Health", "Collision Damage", "Maximum Damage", "Passive Damage", "Mark Detonation Damage", "Beam Damage", "Shield Absorption", "Damage Absorption", "Shield Health", "Shield Health "];
+		var label = spell.leveltip.label;
+		var index = -1;
+		
+		for (var i=0; i < labels.length; i++){
+			index = label.indexOf(labels[i]);
+			if (index > -1){
+				spell.eff = label[index];
+				break;
+			}
+		}
+
+		if (index > -1){
+			spell.effectVal = spell.effectBurn[index+1];
+		}else{
+			spell.effectVal = spell.effectBurn[1];
+		}
 	}
 
 	function start(){
 		$scope.inProgress = true;
 		$scope.done = false;
 		$scope.errMsg = "";
+		$scope.champions = [];
+		$scope.spells = [];
 	}
 
 	function end(){
@@ -57,8 +129,8 @@ app.controller('mainController', function($scope, $http, Static){
 		$scope.done = true;
 	}
 
-	$scope.test = function(){
-		
+	$scope.test = function(spell){
+		console.log(spell);
 	};
 
 });
